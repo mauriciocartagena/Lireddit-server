1755ab2e3e30c9d01161f8abf7942a0e5159fea1
Mauricio Cartagena <mc973220@gmail.com>
2021-04-13 23:02:22 -0400

    creando tablas con PostgreSql

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,17 @@
+import { __prod__ } from "./constants";
+import { MikroORM } from "@mikro-orm/core";
+import path from "path";
+import { Post } from "./entities/Post";
+
+export default {
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.ts$/, // reg
+  },
+  entities: [Post],
+  dbName: "lireddit",
+  type: "postgresql",
+  debug: !__prod__,
+} as Parameters<typeof MikroORM.init>[0];
